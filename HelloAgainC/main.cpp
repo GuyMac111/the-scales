ac1f8acd53dfbd78dde544d706179d2fb5fb1d7c
Guy McLean <guy.mclean@superawesome.com>
2023-11-13 17:02:02 +0000

    Initial Commit

---
+++ HelloAgainC/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloAgainC
+//
+//  Created by Guy McLean on 13/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
